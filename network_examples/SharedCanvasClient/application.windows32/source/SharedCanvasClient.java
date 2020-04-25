54d27c58ff4ead1519e6de2d6d33400ff4b6cd33
tiedyeguy <jeis1901@mpsedu.org>
2020-04-24 23:08:08 -0500

    First commit

---
+++ network_examples/SharedCanvasClient/application.windows32/source/SharedCanvasClient.java
@@ -0,0 +1,74 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import processing.net.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SharedCanvasClient extends PApplet {
+
+/**
+ * Shared Drawing Canvas (Client) 
+ * by Alexander R. Galloway. 
+ * 
+ * The Processing Client class is instantiated by specifying a remote 
+ * address and port number to which the socket connection should be made. 
+ * Once the connection is made, the client may read (or write) data to the server.
+ * Before running this program, start the Shared Drawing Canvas (Server) program.
+ */
+
+
+
+
+Client c;
+String input;
+int data[];
+
+public void setup() 
+{
+  
+  background(204);
+  stroke(0);
+  frameRate(5); // Slow it down a little
+  // Connect to the server's IP address and port
+  c = new Client(this, "67.220.19.208", 12345); // Replace with your server's IP and port
+}
+
+public void draw() 
+{
+  if (mousePressed == true) {
+    // Draw our line
+    stroke(255);
+    line(pmouseX, pmouseY, mouseX, mouseY);
+    // Send mouse coords to other person
+    c.write(pmouseX + " " + pmouseY + " " + mouseX + " " + mouseY + "\n");
+  }
+  // Receive data from server
+  if (c.available() > 0) {
+    input = c.readString();
+    input = input.substring(0, input.indexOf("\n")); // Only up to the newline
+    data = PApplet.parseInt(split(input, ' ')); // Split values into an array
+    // Draw line using received coords
+    stroke(0);
+    line(data[0], data[1], data[2], data[3]);
+  }
+}
+  public void settings() {  size(450, 255); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SharedCanvasClient" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
